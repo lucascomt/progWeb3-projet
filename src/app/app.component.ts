f99e711be2fe5371d46e2388fbb6893bf26883ee
Óðinn Qc <lucas@odinn.biz>
2024-10-19 10:17:00 -0400

    modif des liens vers sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { QuiSuisJeComponent } from "./qui-suis-je/qui-suis-je.component";
 import { ProgrammationComponent } from "./programmation/programmation.component";
+import { ServeurJeuxComponent } from "./serveur-jeux/serveur-jeux.component";
 import { BddComponent } from "./bdd/bdd.component";
 import { PhotographieComponent } from "./photographie/photographie.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, QuiSuisJeComponent, ProgrammationComponent, BddComponent, PhotographieComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, QuiSuisJeComponent, ProgrammationComponent, ServeurJeuxComponent, BddComponent, PhotographieComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
